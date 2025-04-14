91b8fecb4f882cabe9ffd5c4d8efbac169092bdc
Rajiv singh <innovaterajiv@gmail.com>
2025-04-14 21:54:32 +0530

    Add project files.

---
+++ Controllers/LoginAPIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myGITAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginAPIController : ControllerBase
+    {
+
+        [HttpGet("UserList")]
+        public async Task<IActionResult> GetUserList()
+        {
+            List<string> userList = new List<string>
+            {
+                "user1",
+                "user2",
+                "user3"
+            };
+           
+
+            var response = new
+            {
+                success = true,
+                message = "User list fetched successfully.",
+                data = userList
+            };
+
+            return Ok(response);
+        }
+    }
+}
